2c5b392f722830c6e7ef7b7d75b64fccb15dc72b
Amey Kadwe <143980556+Ameykadwe19@users.noreply.github.com>
2026-07-23 00:52:05 +0530

    Add: boats-to-save-people - submission-6

---
+++ Data Structures & Algorithms/boats-to-save-people/submission-6.js
@@ -0,0 +1,23 @@
+class Solution {
+    /**
+     * @param {number[]} people
+     * @param {number} limit
+     * @return {number}
+     */
+    numRescueBoats(people, limit) {
+        let left = 0;
+        let right = people.length - 1;
+        let boats = 0;
+        people.sort((a, b) => a - b);
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                
+                left++;
+                
+            } 
+            boats++
+            right--
+        }
+        return boats;
+    }
+}
